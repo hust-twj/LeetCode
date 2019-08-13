5a85d73592ccfe99a3be0eca5428f2ce9fe21113
tangwenjing <hust_twj@163.com>
2019-08-13 15:02:20 +0800

    [feature]加入链表倒数删除

---
+++ src/RemoveNthFromEnd_19.java
@@ -0,0 +1,21 @@
+import tool.ListNode;
+
+/**
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
+ * 19. 删除链表的倒数第N个节点
+ * Created by Wenjing.Tang on 2019-08-13.
+ */
+public class RemoveNthFromEnd_19 {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode node = new ListNode(5);
+        return node;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("haha2");
+
+        System.out.println(2);
+    }
+
+}
---
+++ src/tool/ListNode.java
@@ -0,0 +1,17 @@
+package tool;
+
+/**
+ * 链表节点
+ * Created by Wenjing.Tang on 2019-08-13.
+ */
+public class ListNode {
+
+    public int val;
+
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+}
