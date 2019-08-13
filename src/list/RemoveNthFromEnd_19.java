04b831686f5ce2eae06130faef37458ed1bde0b4
tangwenjing <hust_twj@163.com>
2019-08-13 21:28:02 +0800

    [fix]完善列表倒数删除

--- src/list/RemoveNthFromEnd_19.java
+++ src/list/RemoveNthFromEnd_19.java
@@ -2,8 +2,6 @@ package list;
 
 import tool.ListNode;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
@@ -27,80 +25,39 @@ public class RemoveNthFromEnd_19 {
         node3.next = node4;
         node4.next = node5;
 
-       // ListNode.printListNode(head);
-        System.out.println(head.toString());
-
-        ListNode removedListNode = removeNthFromEnd(head, 2);
-        ListNode.printListNode(removedListNode);
-
         ListNode.printListNode(head);
 
-//        List<Integer> list = new ArrayList<>();
-//        list.add(1);
-//        list.add(2);
-//        list.add(3);
-//        list.add(4);
-//        list.add(5);
-//         ListNode.printListNode(convertList2Node(list));
+        ListNode removedListNode = removeNthFromEnd2(head, 1);
+        ListNode.printListNode(removedListNode);
+
     }
 
     public static ListNode removeNthFromEnd2(ListNode head, int n) {
         if (head == null) {
             return head;
         }
-        ListNode start;
-        ListNode end;
+        //起始指针
+        ListNode start = head;
+        //终止指针
+        ListNode end = head;
 
-        return head;
-    }
-
-    public static ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null) {
-            return head;
-        }
-        ListNode leader = head;
-        ListNode follower = head;
-        //领导先行n步
+        //终止指针先走n步，当走到最后一个节点时，起始指针的下一个节点即为所要删除的节点
         for (int i = 0; i < n + 1; i++) {
-            if (leader == null) {
+            if (end == null) {
                 return head.next;
             }
-            leader = leader.next;
+            end = end.next;
         }
-        //同步前进直到领导到末尾
-        while (leader != null) {
-            leader = leader.next;
-            follower = follower.next;
+
+        //双指针同步走
+        while (end != null) {
+            end = end.next;
+            start = start.next;
         }
-        //删除追随者处的节点
-        follower.next = follower.next.next;
+        //删除起始指针的后一个节点位置
+        start.next = start.next.next;
+
         return head;
-    }
-
-    public static List<Integer> convertNode2List(ListNode listNode) {
-        List<Integer> list = new ArrayList<>();
-        if (listNode == null) {
-            return list;
-        }
-        list.add(listNode.val);
-        while (listNode.next != null) {
-            list.add(listNode.next.val);
-        }
-        return list;
-    }
-
-    public static ListNode convertList2Node(List<Integer> list) {
-        ListNode listNode = new ListNode(-1);
-        if (list == null || list.size() == 0) {
-            return listNode;
-        }
-//        listNode = new ListNode(list.get(0));
-//        if (list.size() > 1) {
-//            for (int i = 0; i < list.size(); i++) {
-//                listNode.next.val = list.get(1);
-//            }
-//        }
-        return listNode;
     }
 
 }
--- src/list/ReverseList.java
+++ src/list/ReverseList.java
@@ -1,8 +1,13 @@
 package list;
 
 /**
- * Description ：链表反转
+ * Description ：单向链表反转
  * Created by Wenjing.Tang on 2019-08-13.
  */
 public class ReverseList {
+
+    public static void main(String[] args) {
+
+    }
+
 }
