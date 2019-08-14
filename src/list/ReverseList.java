86d2e95f93efcba969bc876a428b91fec15261c2
tangwenjing <hust_twj@163.com>
2019-08-14 20:23:31 +0800

    [feature]增加注释

--- src/list/ReverseList.java
+++ src/list/ReverseList.java
@@ -26,19 +26,21 @@ public class ReverseList {
 
     /**
      * 1、迭代（非递归）
+     * https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC206%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E5%8F%8D%E8%BD%AC%E9%93%BE%E8%A1%A8.md
      * https://blog.csdn.net/FX677588/article/details/72357389
      * https://blog.csdn.net/biezhihua/article/details/79922332
      * https://blog.csdn.net/feliciafay/article/details/6841115
      */
     private static ListNode reverseList(ListNode head) {
-        ListNode newHead = null;
-        while (head != null) {
-            ListNode nextNode = head.next;  //暂存head下一个地址，防止变化指针指向后找不到后续的数
-            head.next  = newHead;//head.next指向前一个空间
-            newHead = head;//新链表的头移动到head，扩长一步链表
-            head = nextNode; //head指向原始链表head指向的下一个空间
+        ListNode pre = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next;  //暂存head下一个地址，防止变化指针指向后找不到后续的数
+            current.next  = pre;//current.next指向前一个空间
+            pre = current;//pre后移
+            current = next; //current后移，准备开启新的循环
         }
-        return newHead;
+        return pre;
     }
 
 }
