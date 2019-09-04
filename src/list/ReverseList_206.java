3ce9a0ee94ab200c924c3538e373eda04236ee34
hust_twj <254062403@qq.com>
2019-09-05 01:21:56 +0800

    翻转链表 递归

--- src/list/ReverseList_206.java
+++ src/list/ReverseList_206.java
@@ -4,6 +4,7 @@ import tool.ListNode;
 
 /**
  * Description ：单向链表反转
+ * https://leetcode-cn.com/problems/reverse-linked-list/
  * Created by Wenjing.Tang on 2019-08-13.
  * 思路：使用两个指针
  * 步骤：
@@ -12,7 +13,7 @@ import tool.ListNode;
  * 3 -> 2 -> 1 -> 4
  * 4 -> 3 -> 2 -> 1
  */
-public class ReverseList {
+public class ReverseList_206 {
 
     public static void main(String[] args) {
 
@@ -31,12 +32,14 @@ public class ReverseList {
      * https://blog.csdn.net/biezhihua/article/details/79922332
      * https://blog.csdn.net/feliciafay/article/details/6841115
      * https://www.jianshu.com/p/bd6a64d36916
+     * https://blog.csdn.net/xyh269/article/details/70238501  图解
      */
     private static ListNode reverseList(ListNode head) {
+        //pre 相当于 哑结点
         ListNode pre = null;
         ListNode current = head;
         while (current != null) {
-            ListNode next = current.next;  //暂存头结点head下一个节点，防止变化指针指向后找不到后续的数
+            ListNode next = current.next;  //暂存头结点head下一个节点，防止变化指针指向后找不到后续的数 导致指针丢失
             current.next  = pre;//current.next指向"上一个节点"，这一步实现了反转
             pre = current;//pre后移
             current = next; //current后移，准备开启新的循环
@@ -45,18 +48,28 @@ public class ReverseList {
     }
 
     /**
-     * 递归
-     * @param head
-     * @return
+     * 2、递归
+     * https://stackoverflow.com/questions/354875/reversing-a-linked-list-in-java-recursively
+     * https://blog.csdn.net/FX677588/article/details/72357389
      */
     // TODO: 2019-08-14  
     private static ListNode reverseList2(ListNode head) {
+        //递归终止条件：当head为空或者只有一个节点时，返回
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode reverseRest = null;
-
+        //递归调用，直到链尾
+        ListNode reverseRest = reverseList2(head.next);
+        //翻转链表指向
+        head.next.next = head;
+        head.next = null;
         return reverseRest;
     }
 
+    /**
+     * 3、栈
+     */
+    private static ListNode reverseList3(ListNode head) {
+        return null;
+    }
 }
---
+++ src/list/SwapPairs_24.java
@@ -0,0 +1,33 @@
+package list;
+
+import tool.ListNode;
+
+/**
+ * 24. 两两交换链表中的节点
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ * @author hust_twj
+ * @date 2019/9/5
+ */
+public class SwapPairs_24 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 非递归方法
+     */
+    public static ListNode swapPairs(ListNode head) {
+
+        return null;
+    }
+
+    /**
+     * 递归方法
+     */
+    public static ListNode swapPairs2(ListNode head) {
+
+        return null;
+    }
+
+}
