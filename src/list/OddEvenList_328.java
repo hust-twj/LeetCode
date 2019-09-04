16ac22e2b6740f3277f852403bf490eb8404d812
hust_twj <254062403@qq.com>
2019-09-05 01:26:28 +0800

    奇偶链表

--- src/list/OddEvenList_328.java
+++ src/list/OddEvenList_328.java
@@ -1,5 +1,7 @@
 package list;
 
+import tool.ListNode;
+
 /**
  * 328 奇偶链表
  * https://leetcode-cn.com/problems/odd-even-linked-list/
@@ -7,4 +9,13 @@ package list;
  * @date 2019/9/5
  */
 public class OddEvenList_328 {
+
+    public static void main(String[] args) {
+
+    }
+    public ListNode oddEvenList(ListNode head) {
+
+        return null;
+    }
+
 }
