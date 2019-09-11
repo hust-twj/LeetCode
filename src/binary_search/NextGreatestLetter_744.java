22f384b6538e275967aa82de53883b5418bfb78e
tangwenjing <hust_twj@163.com>
2019-09-11 15:04:15 +0800

    744. 寻找比目标字母大的最小字母（待完善）

---
+++ src/binary_search/NextGreatestLetter_744.java
@@ -0,0 +1,21 @@
+package binary_search;
+
+/**
+ * https://leetcode-cn.com/problems/find-smallest-letter-greater-than-target/
+ * Description ：744. 寻找比目标字母大的最小字母
+ * Created by Wenjing.Tang on 2019-09-11.
+ */
+public class NextGreatestLetter_744 {
+
+    public static void main(String[] args) {
+        char[] letters = new char[]{'c', 'f', 'j'};
+        char target = 'a';
+        System.out.println(nextGreatestLetter(letters, target));
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+
+        return 'a';
+    }
+
+}
