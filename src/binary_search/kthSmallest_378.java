48a8137032b391d38f65e15bd9e505d21a63ded2
hust_twj <254062403@qq.com>
2019-09-17 00:55:00 +0800

    最小堆排序

--- src/binary_search/kthSmallest_378.java
+++ src/binary_search/kthSmallest_378.java
@@ -1,5 +1,8 @@
 package binary_search;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * https://leetcode-cn.com/problems/kth-smallest-element-in-a-sorted-matrix/
  * Description ：378. 有序矩阵中第 K 小的元素
@@ -9,7 +12,7 @@ public class kthSmallest_378 {
 
     public static void main(String[] args) {
         int[][] intArray = {{1, 5, 9}, {10, 11, 13}, {12, 13, 15}};
-        System.out.println(kthSmallest(intArray, 2));
+        System.out.println(kthSmallest2(intArray, 8));
     }
 
     /**
@@ -22,7 +25,7 @@ public class kthSmallest_378 {
         int right = matrix[row - 1][col - 1];
         while (left < right) {
             // 每次循环都保证第K小的数在start~end之间，当start==end，第k小的数就是start
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
             // 找二维矩阵中<=mid的元素总个数
             int count = findNoBiggerThanMid(matrix, mid, row, col);
             if (count < k) {
@@ -57,6 +60,31 @@ public class kthSmallest_378 {
         return count;
     }
 
-    // TODO: 2019/9/16 最小堆排序
+    /**
+     * 最小堆排序
+     * 建堆：
+     * https://blog.csdn.net/weixin_30363263/article/details/80862578
+     * https://blog.csdn.net/u013309870/article/details/71189189
+     */
+    public static int kthSmallest2(int[][] matrix, int k) {
 
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (maxHeap.size() < k) {
+                    maxHeap.add(matrix[i][j]);
+                } else if (maxHeap.peek() > matrix[i][j]) {
+                    maxHeap.poll();
+                    maxHeap.add(matrix[i][j]);
+                }
+            }
+        }
+        return maxHeap.peek();
+    }
 }
