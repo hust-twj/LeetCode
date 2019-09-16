65238f8260ab77dc67642e5fa36647b08c8e309e
tangwenjing <hust_twj@163.com>
2019-09-16 21:42:54 +0800

    378. 有序矩阵中第 K 小的元素

---
+++ src/binary_search/kthSmallest_378.java
@@ -0,0 +1,20 @@
+package binary_search;
+
+/**
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-sorted-matrix/
+ * Description ：378. 有序矩阵中第 K 小的元素
+ * Created by Wenjing.Tang on 2019-09-16.
+ */
+public class kthSmallest_378 {
+
+    public static void main(String[] args) {
+        int[][] intArray = {{1, 5, 9}, {10, 11, 13}, {12, 13, 1}};
+        System.out.println(kthSmallest(intArray, 13));
+    }
+
+    public static int kthSmallest(int[][] matrix, int k) {
+
+        return 0;
+    }
+
+}
