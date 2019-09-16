1d92599cd7a7c00b06f06f6c7e069abf076ba964
hust_twj <254062403@qq.com>
2019-09-16 23:54:20 +0800

    230. 二叉搜索树中第 K 小的元素

---
+++ src/tree/KthSmallest_230.java
@@ -0,0 +1,17 @@
+package tree;
+
+/**
+ * 230. 二叉搜索树中第 K 小的元素
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+ * @author hust_twj
+ * @date 2019/9/11
+ * 解答：
+ * 根据特点，二叉搜索树已经是“排序”过的，根据“中序”遍历，即可求第 K 小的元素
+ */
+public class KthSmallest_230 {
+
+    public int kthSmallest(TreeNode root, int k) {
+        return 0;
+
+    }
+}
