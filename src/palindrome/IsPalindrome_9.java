72068f58f2b1c20044a4df966b31a93455fd176f
hust_twj <254062403@qq.com>
2019-09-20 00:20:36 +0800

    不使用字符串处理回文数

--- src/palindrome/IsPalindrome_9.java
+++ src/palindrome/IsPalindrome_9.java
@@ -26,7 +26,6 @@ public class IsPalindrome_9 {
     }
 
     /**
-     * todo
      *
      * 不使用字符串处理
      * @param x
@@ -36,12 +35,17 @@ public class IsPalindrome_9 {
         if (x < 0) {
             return false;
         }
-        while (x != 0) {
-            int last = x % 10;
-            x = last;
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            //得到反转的
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
         }
-        return false;
-    }
 
+        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+        return x == revertedNumber || x == revertedNumber/10;
+    }
 
 }
