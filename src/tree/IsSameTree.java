cb8a6ce68a4f5e8aae6bd8eadcc450b122bde710
tangwenjing <hust_twj@163.com>
2019-09-27 18:15:44 +0800

    使用栈

--- src/tree/IsSameTree.java
+++ src/tree/IsSameTree.java
@@ -1,12 +1,15 @@
 package tree;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
- *
  * 100. 相同的树
  * https://leetcode-cn.com/problems/same-tree/
  * Description ：
  * Created by Wenjing.Tang on 2019-09-27.
- *
+ * <p>
  * 二叉树框架
  * https://leetcode-cn.com/problems/same-tree/solution/xie-shu-suan-fa-de-tao-lu-kuang-jia-by-wei-lai-bu-/
  */
@@ -27,11 +30,22 @@ public class IsSameTree {
 
         treeNode3.left = treeNode6;
 
-        System.out.print(isSameTree(treeNode1, treeNode1));
+        //System.out.print(isSameTree(treeNode1, treeNode1));
+
+        Deque<Integer> stackQ = new LinkedList<>();
+        stackQ.push(1);
+        stackQ.push(2);
+        stackQ.push(3);
+        stackQ.push(4);
+        while (!stackQ.isEmpty()) {
+            System.out.println(stackQ.pop());
+        }
+
     }
 
     /**
      * 递归
+     *
      * @param p
      * @param q
      * @return
@@ -49,6 +63,41 @@ public class IsSameTree {
         }
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 
+    }
+
+    /**
+     * 栈 迭代
+     * @param p
+     * @param q
+     * @return
+     */
+    public static boolean isSameTree2(TreeNode p, TreeNode q) {
+        //使用 Deque 替代 Stack
+        Deque<TreeNode> stackQ = new LinkedList<>();
+        Deque<TreeNode> stackP = new LinkedList<>();
+        stackP.push(p);
+        stackQ.push(q);
+
+        while (!stackQ.isEmpty() && !stackP.isEmpty()) {
+            TreeNode treeNodeP = stackP.pop();
+            TreeNode treeNodeQ = stackQ.pop();
+
+            if (treeNodeP == null && treeNodeQ == null) {
+                continue;
+            }
+            if (treeNodeP != null && treeNodeQ != null && treeNodeP.val == treeNodeQ.val) {
+                stackP.push(treeNodeP.left);
+                stackP.push(treeNodeP.right);
+
+                stackQ.push(treeNodeQ.left);
+                stackQ.push(treeNodeQ.right);
+
+            }else {
+                return false;
+            }
+
+        }
+        return stackP.isEmpty() && stackQ.isEmpty();
     }
 
 }
