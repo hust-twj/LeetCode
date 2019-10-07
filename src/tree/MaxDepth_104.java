a64d90585bdb53cdda6f318324673d6c13e9d110
hust_twj <254062403@qq.com>
2019-10-07 19:13:04 +0800

    104. 二叉树的最大深度

---
+++ src/tree/MaxDepth_104.java
@@ -0,0 +1,30 @@
+package tree;
+
+/**
+ * 104. 二叉树的最大深度
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ * @author hust_twj
+ * @date 2019/10/7
+ */
+public class MaxDepth_104 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 二叉树的的深度等于左 / 右子树的深度的较大值加 1。即 maxDepth(root)=Max(maxDepth(root->left),maxDepth(root->right))+1
+     *
+     * @param root
+     * @return
+     */
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+}
