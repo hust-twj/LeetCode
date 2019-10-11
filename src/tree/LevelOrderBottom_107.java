8326ff2574e78553b50cc0d9aed22a6863a1c6f5
hust_twj <254062403@qq.com>
2019-10-11 23:37:27 +0800

    二叉树层次遍历（队列）

--- src/tree/LevelOrderBottom_107.java
+++ src/tree/LevelOrderBottom_107.java
@@ -1,6 +1,6 @@
 package tree;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 107. 二叉树的层次遍历 II
@@ -10,9 +10,39 @@ import java.util.List;
  */
 public class LevelOrderBottom_107 {
 
-    // TODO: 2019/10/10
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        //队列 add / poll
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> sub = new ArrayList<>();
+            //遍历每一层
+            int count = queue.size();
+            // 注意：
+            // 不能写成 for (int i = 0; i < queue.size(); i++)
+            // 不然queue.poll()后queue的大小会变化导致结果错误
+            for (int i = 0; i < count; i++) {
+                TreeNode treeNode = queue.poll();
+                if (treeNode != null) {
+                    sub.add(treeNode.val);
+                    if (treeNode.left != null) {
+                        queue.add(treeNode.left);
+                    }
+                    if (treeNode.right != null) {
+                        queue.add(treeNode.right);
+                    }
+                }
+            }
+            //注意顺序，添加到第0个位置
+            result.add(0, sub);
+        }
+        //Collections.reverse(result);
+        return result;
     }
 
 }
