543e4ba663b29504966eaa9f4fd3d3c2a240a79c
tangwenjing <hust_twj@163.com>
2019-10-29 11:50:16 +0800

    5. 最长回文子串 动态规划（二维）

--- src/string/LongestPalindrome_5.java
+++ src/string/LongestPalindrome_5.java
@@ -15,13 +15,12 @@ public class LongestPalindrome_5 {
     public static void main(String[] args) {
         String s = "lejyqjcpluiggwlmnumqpxljlcwdsirzwlygexejhvojztcztectzrepsvwssiixfmpbzshpilmojehqyqpzdylxptsbvkgatzdlzphohntysrbrcdgeaiypmaaqilthipjbckkfbxtkreohabrjpmelxidlwdajmkndsdbbaypcemrwlhwbwaljacijjmsaqembgtdcskejplifnuztlmvasbqcyzmvczpkimpbbwxdtviptzaenkbddaauyvqppagvqfpednnckooxzcpuudckakutqyknuqrxjgfdtsxsoztjkqvfvelrklforpjnrbvyyvxigjhkjmxcphjzzilvbjbvwiwnnkbmboiqamgoimujtswdqesighoxsprhnsceshotakvmoxqkqjvbpqucvafiuqwmrlfjpjijbctfupywkbawquchbclgvhxbanybret";
         // System.out.println(s.substring(0, s.length()-1));
-        System.out.println(longestPalindrome(s));
+        System.out.println(longestPalindrome1(s));
     }
 
     /**
      * 暴力法（超时）
      * 滑动窗口思想
-     *
      */
     public static String longestPalindrome(String s) {
         if (s == null) {
@@ -47,7 +46,7 @@ public class LongestPalindrome_5 {
     /**
      * 动态规划（二维）
      * https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zhong-xin-kuo-san-dong-tai-gui-hua-by-liweiwei1419/
-     *
+     * 回文串的状态转移方程：dp[l][r] = s[l]==s[r] && dp[l+1]dp[r-1]
      * @param s
      * @return
      */
@@ -58,12 +57,24 @@ public class LongestPalindrome_5 {
         if (s.length() <= 1) {
             return s;
         }
-        for (int i = 0; i < s.length(); i++) {
-
+        //回文串的长度
+        int palindromeLength = 1;
+        String longestPalindromeStr = s.substring(0, 1);
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int right = 1; right < s.length(); right++) {
+            for (int left = 0; left < right; left++) {
+                // right - left <= 2 || dp[left + 1][right - 1] 或运算是精华
+                // right - left <= 2 说明左右中间，最多只有一个元素，那么一定是回文，就短路
+                if (s.charAt(left) == s.charAt(right) && (right - left <= 2 || dp[left + 1][right - 1])) {
+                    dp[left][right] = true;
+                    if (palindromeLength < right - left + 1) {
+                        palindromeLength = right - left + 1;
+                        longestPalindromeStr = s.substring(left, right + 1);
+                    }
+                }
+            }
         }
-
-        return null;
-
+        return longestPalindromeStr;
     }
 
     /**
