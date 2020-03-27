394f4506b8825c3e156c14918e7a091b5142e61e
tangwenjing <hust_twj@163.com>
2020-03-28 00:48:16 +0800

    [剑指offer]第 5 题：从尾到头打印链表

--- src/sword_to_offer/Sword2Offer_5.java
+++ src/sword_to_offer/Sword2Offer_5.java
@@ -16,10 +16,13 @@ public class Sword2Offer_5 {
 
         ListNode.printListNode(listNode);
 
-        printFromEnd2Head(listNode);
+        printFromEnd2Head2(listNode);
 
     }
 
+    /**
+     * 基于 栈
+     */
     private static void printFromEnd2Head(ListNode head) {
         Stack<Integer> stack = new Stack<>();
         if (head == null) {
@@ -31,7 +34,17 @@ public class Sword2Offer_5 {
             head = head.next;
         }
         while (!stack.isEmpty()) {
-            System.out.println(stack.pop());
+            System.out.print(stack.pop() + " -> ");
         }
+    }
+    /**
+     * 由于栈的本质也是递归，因此，本题可以用递归来实现
+     */
+    private static void printFromEnd2Head2(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        printFromEnd2Head2(head.next);
+        System.out.print(head.val +  " -> ");
     }
 }
